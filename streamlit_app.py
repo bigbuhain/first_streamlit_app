066520a48356e65117001046248ad53aaab34925
bigbuhain <64065605+bigbuhain@users.noreply.github.com>
2022-08-04 15:28:24 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 import pandas
 
-streamlist.title('Build Your Own Fruit Smoothie')
+streamlit.title('Build Your Own Fruit Smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
