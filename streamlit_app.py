eac82d8fc4ca658add225f08788d95be6133c872
bigbuhain <64065605+bigbuhain@users.noreply.github.com>
2022-08-06 08:16:16 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,12 +31,10 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# Normalized JSON response from fruityvice_response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# Normalized JSON response from fruityvice_response
-#fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
-
 #return response as table
-#streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 
